b19e12fc4f7b60cb07025d956483c366bf07cf7b
Adam Matthews <a.matthews42@outlook.com>
2022-05-27 17:53:58 +0100

    Initial commit

---
+++ CelestialBody.gd
@@ -0,0 +1,35 @@
+tool
+extends MeshInstance
+
+# Declare member variables here. Examples:
+export (int) var cb_id = 0 setget set_id;
+export (float) var cb_mass = 1.0 setget set_mass;
+export (float) var cb_radius = 1.0 setget set_radius;
+export (Vector3) var initial_velocity = Vector3(0,0,0) setget set_initial_velocity;
+
+func get_id():
+	return cb_id;
+	
+func set_id(val):
+	cb_id = val;
+	
+func get_mass():
+	return cb_mass;
+	
+func set_mass(val):
+	cb_mass = val;
+		
+func get_radius():
+	return cb_radius;
+	
+func set_radius(val):
+	cb_radius = val
+	
+	get_mesh().set_radius(val)
+	get_mesh().set_height(val*2)
+	
+func get_initial_velocity():
+	return initial_velocity;
+
+func set_initial_velocity(val):
+	initial_velocity = val;
---
+++ SolarSystem.gd
@@ -0,0 +1,40 @@
+extends Spatial
+
+# Declare member variables here. Examples:
+var fixed_time_step = 0.02;
+var time_since_last = 0.0;
+var celestial_bodies = [];
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for node in get_tree().get_nodes_in_group("celestial_bodies"):
+		celestial_bodies.append(node);	
+
+		NBodySystem.add_celestial_body(
+			node.get_id(),
+			node.get_mass(),
+			node.get_radius(),
+			node.get_initial_velocity(),
+			node.get_global_transform().origin
+		)
+		
+		print(node.get_id())
+		print(node.get_mass())
+		print(node.get_radius())
+		print(node.get_initial_velocity())
+		print(node.get_global_transform().origin)
+		
+	print(celestial_bodies)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	time_since_last += delta;
+	if(time_since_last < fixed_time_step):
+		pass
+		
+	NBodySystem.run_simulation_step(1)
+	
+	for body in celestial_bodies:
+		var position = NBodySystem.get_body_position(int(body.get_id()));
+		print(position)
+		body.transform.origin = position;
